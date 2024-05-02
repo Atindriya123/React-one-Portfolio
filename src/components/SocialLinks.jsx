28a517e6914b8466e66b1848ec4097d854846d05
Atindriya mondal <atindriyamondal1@gmail.com>
2024-05-02 15:38:54 +0530

    new commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "https://drive.google.com/file/d/1kjxQrpTomTwMoC5_nNPZqy2AHgk44x2D/view?usp=sharing", // Provide the actual URL for the resume
+      href: "https://drive.google.com/file/d/1zxU0XosuqpOYSP0b56MCzCi6QQUSfOu-/view?usp=sharing", // Provide the actual URL for the resume
       style: "rounded-br-md",
       download: true,
     },
